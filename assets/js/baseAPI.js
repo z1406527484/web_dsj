2b48a6cb277eb628fca4160e30fdd5866ebadd20
zfj <1406527484@qq.com>
2022-04-14 21:34:13 +0800

    index开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,14 @@
 $.ajaxPrefilter(function(options) {
     options.url = 'http://www.liulongbin.top:3007' + options.url
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+    options.complete = function(res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1. 清空本地存储中的 token
+            localStorage.removeItem('token')
+                // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,57 @@
+$(function() {
+    var layer = layui.layer
+    getUserInfo()
+    $('#btnLogout').on('click', function() {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' },
+            function(index) {
+                //do something
+                // 1. 清空本地存储中的 token
+                localStorage.removeItem('token')
+                    // 2. 重新跳转到登录页面
+                location.href = '/login.html'
+
+                // 关闭 confirm 询问框
+                layer.close(index)
+            })
+    })
+})
+
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        /* headers: {
+            Authorization: localStorage.getItem('token') || ''
+        }, */
+        success: function(res) {
+            if (res.status !== 0) {
+                return layer.msg('请求失败')
+            }
+            renderAvatar(res.data)
+        },
+        /* complete: function(res) {
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1. 清空本地存储中的 token
+                localStorage.removeItem('token')
+                    // 2. 重新跳转到登录页面
+                location.href = '/login.html'
+            }
+        } */
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
